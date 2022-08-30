594aa4b59c57dbc633c961a3eeffa45b54acf47b
florianeBhz <behanzinf@gmail.com>
2022-08-30 20:19:19 +0200

    code refactoring and hyperparameter tuning

--- src/sentimentanalysisapp/fastapibackend/inference.py
+++ src/sentimentanalysisapp/fastapibackend/inference.py
@@ -1,6 +1,8 @@
 from distutils.command.config import config
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import numpy as np
+
+"""
 import boto3
 import config
 
@@ -10,19 +12,10 @@ BUCKET_NAME = config.BUCKET_NAME
 FILE_NAME = "sentimentanalysismodel/config.json"
 DEST = "./config.json"
 s3 = boto3.client('s3',aws_access_key_id=AWS_ACCESS_KEY_ID , aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
-response = s3.list_buckets()
-buckets = [bucket['Name'] for bucket in response['Buckets']]
-
-# Print out the bucket list
-print("Bucket List: %s" % buckets)
-s3.download_file(BUCKET_NAME,FILE_NAME,DEST)
-
-#aws s3api get-object --bucket DOC-EXAMPLE-BUCKET1 --key dir/my_images.tar.bz2 my_images.tar.bz2
 """ 
-# Load trained model
 
-#"./sentimentanalysismodel"
+# Load trained model
+model_path = "./sentimentanalysismodel"
 model_loaded = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=3)
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
@@ -32,4 +25,3 @@ def pipeline(text):
     pred_np = pred[0][0].detach().numpy()
     res = np.argmax(pred_np)
     return res
-"""
